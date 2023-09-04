ec6e4615e992095ae5100462023cdfd1766c434b
Miguel Corbos <corbos1995@gmail.com>
2023-09-04 01:12:53 -0400

    TestCallback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const supabase = createRouteHandlerClient<Database>({ cookies });
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-
+  console.log(code);
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }
