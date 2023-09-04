186ee97d9bc38a3d75c1ee13b363cb29036ab1aa
Miguel Corbos <corbos1995@gmail.com>
2023-09-04 01:09:44 -0400

    Function for vercel

--- app/login/auth-google-button.tsx
+++ app/login/auth-google-button.tsx
@@ -9,14 +9,26 @@ function AuthGoogleButton() {
   const supabase = createClientComponentClient<Database>();
   const router = useRouter();
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   async function GoogleLogin() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `${getURL()}/auth/callback`,
       },
     });
-    console.log(location.origin);
+
     router.refresh();
   }
 
